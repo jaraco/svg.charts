7aff44925e712f4e279f1dbfbb05f08c935eb1a8
Jason R. Coombs <jaraco@jaraco.com>
2016-03-22 12:42:55 -0400

    Add test demonstrating the purpose of __all__ in svg.charts

---
+++ tests/test_package.py
@@ -0,0 +1,10 @@
+def test_import_star():
+	"""
+	Test that `from svg.charts import *` imports all the modules
+	"""
+	mod_src = 'from svg.charts import *\n'
+	code = compile(mod_src, 'test_import_star_module.py', 'exec')
+	ns = {}
+	exec(code, ns)
+	assert 'graph' in ns
+	assert 'plot' in ns
