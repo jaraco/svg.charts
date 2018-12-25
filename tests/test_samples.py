01ca12d6b33af457ae2c431a6f975ed1deffe4c7
Jason R. Coombs <jaraco@jaraco.com>
2018-12-25 09:35:54 -0500

    Convert test generation call to parametrized test. Fixes compatibility with pytest 4.

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,11 +1,11 @@
+import pytest
+
 import samples
 
 
-def pytest_generate_tests(metafunc):
-	if "sample" in metafunc.funcargnames:
-		for name, chart in samples.generate_samples():
-			metafunc.addcall(funcargs=dict(sample=chart))
-
-
+@pytest.mark.parametrize(
+	'sample',
+	[chart for name, chart in samples.generate_samples()],
+)
 def test_sample(sample):
 	sample.burn()
